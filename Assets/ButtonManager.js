86a12823df149e512263b687f82363a1b91a6ad7
Isabel <34gomez.i@gmail.com>
2015-11-11 13:48:54 -0800

    Added buttons that actually have working functions like adding & subtracting from score!

---
+++ Assets/ButtonManager.js
@@ -0,0 +1,40 @@
+﻿#pragma strict
+
+public var PlayButton : UI.Button;
+public var SButton : UI.Button;
+public var TButton : UI.Button;
+//public var ScoreWord : UI.Text;
+public var ScoreText : UI.Text;
+private var NumScore : int;
+
+function Start () {
+	
+	NumScore = 0;
+	//IncreaseScore ();
+
+}
+
+function Update () {
+
+}
+
+function Play () {
+	
+	print("F U");
+}
+
+function IncreaseScore () {
+
+	NumScore = NumScore + 10;
+	ScoreText.text = NumScore.ToString ();
+ 
+ }
+
+ function DecreaseScore () {
+	//everytime TButton is clicked, score decreases.
+ 	
+ 	NumScore = NumScore - 10;
+ 	ScoreText.text = NumScore.ToString ();
+
+ }
+
