d2b9d4347b0fb7c1fe9effd3f5defd6b674134ae
Isabel <34gomez.i@gmail.com>
2015-11-13 12:47:31 -0800

    Kim has successfully kried.

--- Assets/ButtonManagerCs.cs
+++ Assets/ButtonManagerCs.cs
@@ -6,8 +6,11 @@ using UnityEngine.UI;
 public class ButtonManagerCs : MonoBehaviour {
 
 public Button FryButton;
-public Button KylieButton; 
+public Button KylieButton;
 public Button KimButton; 
+public GameObject KylieGameObject;
+public GameObject KryingKim;
+public GameObject FryFryFry; 
 //public var ScoreWord : UI.Text;
 public Text ScoreText;
 private int NumScore; 
@@ -24,17 +27,12 @@ private int NumScore;
 	}
 
 public void IncreaseScore () {
-	//Everytime Kylie's doublechin is pressed, score increases.
-	print("FU");
-	
-	NumScore = NumScore + 10;
+	NumScore = NumScore + 18;
 	ScoreText.text = NumScore.ToString ();
  
  }
 
  public void DecreaseScore () {
-	//everytime KimButton is clicked, score decreases.
- 	
  	NumScore = NumScore - 10;
  	ScoreText.text = NumScore.ToString ();
 
