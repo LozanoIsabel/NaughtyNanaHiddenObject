08e58831c6bfb7c1a03177f288d6b4a8ad4287c2
Isabel <34gomez.i@gmail.com>
2015-11-13 19:42:00 -0800

    UPDATED HIDDEN OBJECT KIM K GAME

--- Assets/ButtonManagerCs.cs
+++ Assets/ButtonManagerCs.cs
@@ -7,10 +7,17 @@ public class ButtonManagerCs : MonoBehaviour {
 
 public Button FryButton;
 public Button KylieButton;
-public Button KimButton; 
-public GameObject KylieGameObject;
-public GameObject KryingKim;
-public GameObject FryFryFry; 
+public Button KimButton;
+public Button StartButton;
+public Button PterodactylKim;
+public Button Kim2Krying;
+public Button Kanye;
+public GameObject Kim2KryingGO;
+public GameObject PterodactylKimGO;
+public GameObject KanyeGO;
+public GameObject KylieGO;
+public GameObject KryingKimGO;
+public GameObject FryGO; 
 //public var ScoreWord : UI.Text;
 public Text ScoreText;
 private int NumScore; 
@@ -18,22 +25,24 @@ private int NumScore;
 
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	if (!FryButton.isActiveAndEnabled && !KimButton.isActiveAndEnabled && !KylieButton.isActiveAndEnabled)
+		print("Congratulations!");
+		
+
 	}
 
 public void IncreaseScore () {
-	NumScore = NumScore + 18;
+	NumScore = NumScore + 10;
 	ScoreText.text = NumScore.ToString ();
  
  }
 
- public void DecreaseScore () {
- 	NumScore = NumScore - 10;
+ public void SpecialPointScore () {
+ 	NumScore = NumScore + 1688;
  	ScoreText.text = NumScore.ToString ();
 
  }
